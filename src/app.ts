79879242a2638ea55949a382ddfcb991a7073685
Abdi <abdistar16@gmail.com>
2025-11-11 14:38:20 +0300

    feat: api docs route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import authRoutes from "./routes/auth";
 import productRoutes from "./routes/products";
 import orderRoutes from "./routes/order";
+import { swaggerUi, specs } from "./docs/swagger";
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get("/", (req, res) => res.json({ success: true, message: "API ok" }));
 
